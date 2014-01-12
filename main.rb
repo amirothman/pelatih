37d4ea8a7e19c259d1e97e368fd99dae649d51fd
amir <th3go2@gmail.com>
2014-01-13 00:15:17 +0100

    Initialize repo

---
+++ main.rb
@@ -0,0 +1,52 @@
+#Dekat sini, kita nak bina satu perpustakaan nama dia @bibliothek
+
+bibliothek = Array.new
+
+#Bukak folder yg bernama "korpus", cari file yang habis dengan .txt, simpan dalam @bibliothek
+
+Dir.foreach("korpus") do |l|
+   bibliothek << l if l.match(/\.txt$/)
+end
+
+#Untuk bina himpunan ayat2, kita bagi dia nama @korpus
+
+korpus = Array.new
+
+bibliothek.each do |b|
+  korpus = korpus + IO.binread("korpus/"+b).gsub(/\n/," ").split(/\./)
+end
+
+
+#Bina himpunan ayat-ayat pendek
+
+ayat_ayat_pendek = Array.new
+
+korpus.flatten.each do |a|
+
+  #Dekat sini Ruby ada masalah dengan encoding so terpaksa buat mcm ni. rasanya ruby 2.1 dah solve masalah ni
+  a.gsub!(/[\.\,\~\+\\\/\(\)\-\*\"\'\=\%\&\!\]\[\:\;\>\<0-9]+/," ")
+  a.gsub!(/^\s+/,"")
+  a.gsub!(/\s+/," ")
+  
+  keep = true
+
+  if a.split.size > 2 && a.split.size < 8
+
+  a.split(/\s+/).each do |lk|
+  	if lk.size == 1
+        keep = false
+    elsif lk.match(/[^\w]/)
+    	keep = false
+    end
+  end
+  	if keep
+  	  ayat_ayat_pendek << a
+  	end
+  end
+end
+
+ayat_ayat_pendek.uniq.shuffle.each do |w|
+  puts w
+  #kalau nak enter baru keluar ayat baru
+  #gets.chomp
+end
