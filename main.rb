db45c831a8c2d45a71fad909b68299264b343749
amir <th3go2@gmail.com>
2014-01-13 00:52:26 +0100

    seperate loader from main and add Gemfile

--- main.rb
+++ main.rb
@@ -1,48 +1,8 @@
+load "./load.rb"
+
 mode = ARGV[0]
 
-#Dekat sini, kita nak bina satu perpustakaan nama dia @bibliothek
-
-bibliothek = Array.new
-
-#Bukak folder yg bernama "korpus", cari file yang habis dengan .txt, simpan dalam @bibliothek
-
-Dir.foreach("korpus") do |l|
-   bibliothek << l if l.match(/\.txt$/)
-end
-
-#Untuk bina himpunan ayat2, kita bagi dia nama @korpus
-
-korpus = Array.new
-
-bibliothek.each do |b|
-  korpus = korpus + IO.binread("korpus/"+b).gsub(/\n/," ").split(/\./)
-end
-
-
-#Bina himpunan ayat-ayat pendek
-
-ayat_ayat_pendek = Array.new
-
-korpus.flatten.each do |a|
-
-  #Dekat sini Ruby ada masalah dengan encoding so terpaksa buat mcm ni. rasanya ruby 2.1 dah solve masalah ni
-  a.gsub!(/[\.\,\~\+\\\/\(\)\-\*\"\'\=\%\&\!\]\[\:\;\>\<0-9]+/," ")
-  a.gsub!(/^\s+/,"")
-  a.gsub!(/\s+/," ")
-  
-  keep = true
-
-  if a.split.size > 2 && a.split.size < 8
-
-    a.split(/\s+/).each do |lk|
-      unless lk.size == 1 || lk.match(/[^\w]/)
-        ayat_ayat_pendek << a
-      end
-    end
-  end
-end
-
-ayat_ayat_pendek.uniq.shuffle.each do |w|
+@ayat_ayat_pendek.uniq.shuffle.each do |w|
   puts w
   #kalau nak enter baru keluar ayat baru
   #gets.chomp
